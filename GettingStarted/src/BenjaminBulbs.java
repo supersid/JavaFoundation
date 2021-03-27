8275a49852717401af8a85040a119664827ad20f
Siddharth gulati <sid441.sg@gmail.com>
2021-03-27 15:19:04 +0530

    Benjamin Bulbs

---
+++ GettingStarted/src/BenjaminBulbs.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BenjaminBulbs {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		scn.close();
+		
+		for(int i = 1; i * i < n; i++) {
+			System.out.println(i * i);
+		}
+	}
+}
