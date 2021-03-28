0312ce76379db99de71e7f589de7588f553b9558
Siddharth gulati <sid441.sg@gmail.com>
2021-03-28 17:52:46 +0530

    pattern18 real hourglass

---
+++ patterns/patterns/pattern18.java
@@ -0,0 +1,39 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern18 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		scn.close();
+
+		int space = 0;
+		int star = n;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= space; j++) {
+				System.out.print("\t");
+			}
+			for (int j = 1; j <= star; j++) {
+				if(i > 1 && i <= n/2 && j > 1 && j < star) {
+					System.out.print("\t");
+				} else {					
+					System.out.print("*\t");
+				}
+			}
+			for (int j = 1; j <= space; j++) {
+				System.out.print("\t");
+			}
+			if(i <= n/2) {
+				space++;
+				star -=2;
+			} else {
+				star +=2;
+				space--;
+			}
+			System.out.println();
+		}
+	}
+
+}
