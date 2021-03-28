dd2594b4023220bd3370f22ec2d13e62f219086d
Siddharth gulati <sid441.sg@gmail.com>
2021-03-28 16:01:07 +0530

    pattern15

---
+++ patterns/patterns/pattern15.java
@@ -0,0 +1,42 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern15 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		int n = scn.nextInt();
+		scn.close();
+		int space = n / 2;
+		int star = 1;
+		int printVal = 1;
+
+		for (int i = 1; i <= n; i++) {
+			if (i <= n / 2 + 1) {
+				printVal = i;
+			}
+
+			for (int j = 1; j <= space; j++) {
+				System.out.print("\t");
+			}
+			for (int k = 1; k <= star; k++) {
+				System.out.print(printVal + "\t");
+				if (k <= star / 2) {
+					printVal++;
+				} else {
+					printVal--;
+				}
+			}
+			if (i <= n / 2) {
+				space--;
+				star += 2;
+			} else {
+				space++;
+				star -= 2;
+			}
+			System.out.println();
+		}
+	}
+}
