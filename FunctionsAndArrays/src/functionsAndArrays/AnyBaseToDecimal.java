69be9e839fe46e333d1d3df37c6832b3456f53b4
Siddharth gulati <sid441.sg@gmail.com>
2021-03-28 20:22:22 +0530

    Any base to decimal

---
+++ FunctionsAndArrays/src/functionsAndArrays/AnyBaseToDecimal.java
@@ -0,0 +1,27 @@
+package functionsAndArrays;
+
+import java.util.Scanner;
+
+public class AnyBaseToDecimal {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int b = scn.nextInt();
+		int d = getValueIndecimal(n, b);
+		System.out.println(d);
+	}
+
+	public static int getValueIndecimal(int n, int b) {
+		int rv = 0;
+		int place = 0;
+		while (n != 0) {
+			int rem = n % 10;
+			rv = rv + rem * (int) (Math.pow(b, place));
+			place++;
+			n = n / 10;
+		}
+		return rv;
+	}
+
+}
