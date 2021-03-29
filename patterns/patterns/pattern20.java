1d63f84521b25c8b485da948aca93da81a284a7c
Siddharth gulati <sid441.sg@gmail.com>
2021-03-30 01:03:41 +0530

    Pattern 20

---
+++ patterns/patterns/pattern20.java
@@ -0,0 +1,25 @@
+// Code to print 'W' using *
+
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern20 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		scn.close();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j == 1 || j == n || (i > n / 2 && (i == j || i + j == n + 1))) {
+					System.out.print("*\t");
+				} else {
+					System.out.print("\t");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
