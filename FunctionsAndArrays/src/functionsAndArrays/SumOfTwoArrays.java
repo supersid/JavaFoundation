10f3f719d8279c16741690eaeb16282fa0580553
Siddharth gulati <sid441.sg@gmail.com>
2021-03-29 13:41:13 +0530

    Sum of two arrays (individual elements)

---
+++ FunctionsAndArrays/src/functionsAndArrays/SumOfTwoArrays.java
@@ -0,0 +1,52 @@
+package functionsAndArrays;
+
+import java.util.Scanner;
+
+public class SumOfTwoArrays {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n1 = scn.nextInt();
+		int[] arr1 = new int[n1];
+		for (int i = 0; i < arr1.length; i++) {
+			arr1[i] = scn.nextInt();
+		}
+		int n2 = scn.nextInt();
+		int[] arr2 = new int[n2];
+		for (int i = 0; i < arr2.length; i++) {
+			arr2[i] = scn.nextInt();
+		}
+		int[] sum = new int[n1 > n2 ? n1 : n2];
+		sum = sumOfArray(arr1, arr2, sum);
+		for (int val : sum) {
+			System.out.println(val);
+		}
+		scn.close();
+	}
+
+	public static int[] sumOfArray(int[] arr1, int[] arr2, int[] sum) {
+		int c = 0;
+		int i = arr1.length - 1;
+		int j = arr2.length - 1;
+		int k = sum.length - 1;
+		while (k >= 0) {
+			int d = c;
+			if (i >= 0) {
+				d = d + arr1[i];
+			}
+			if (j >= 0) {
+				d = d + arr2[j];
+			}
+			c = d / 10;
+			d = d % 10;
+			sum[k] = d;
+			i--;
+			j--;
+			k--;
+		}
+		if(c != 0) {
+			System.out.println(c);
+		}
+		return sum;
+	}
+}
