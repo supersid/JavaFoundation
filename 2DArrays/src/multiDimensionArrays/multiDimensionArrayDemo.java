feb2188fe79ab1e0b2423deb3338b85a77c78856
Siddharth gulati <sid441.sg@gmail.com>
2021-04-01 23:10:38 +0530

    2D Arrays

---
+++ 2DArrays/src/multiDimensionArrays/multiDimensionArrayDemo.java
@@ -0,0 +1,32 @@
+/*
+ * Display 2D array into matrix format.
+ * This is a demo program to learn how to take input and show output for 2D array.
+ */
+package multiDimensionArrays;
+
+import java.util.Scanner;
+
+public class multiDimensionArrayDemo {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int m = scn.nextInt();
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = scn.nextInt();
+			}
+		}
+		display2DArray(arr);
+	}
+
+	public static void display2DArray(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
