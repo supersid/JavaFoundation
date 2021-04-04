9244d72bae2c3f7e009e25dd1b4171a685f3136e
Siddharth gulati <sid441.sg@gmail.com>
2021-04-04 22:22:40 +0530

    Wave traversal

---
+++ 2DArrays/src/multiDimensionArrays/WaveTraversal.java
@@ -0,0 +1,34 @@
+package multiDimensionArrays;
+
+import java.util.Scanner;
+
+public class WaveTraversal {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int m = scn.nextInt();
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = scn.nextInt();
+			}
+		}
+		scn.close();
+		waveTraversal(arr, n - 1, m - 1);
+	}
+
+	public static void waveTraversal(int[][] arr, int n, int m) {
+		for (int j = 0; j < arr[0].length; j++) {
+			if (j % 2 == 0) {
+				for (int i = 0; i < arr.length; i++) {
+					System.out.println(arr[i][j]);
+				}
+			} else {
+				for (int i = arr.length - 1; i >= 0; i--) {
+					System.out.println(arr[i][j]);
+				}
+			}
+		}
+	}
+}
