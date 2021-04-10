abac494dbf3787c85a394cc9c04217ff9314ab4c
Siddharth gulati <sid441.sg@gmail.com>
2021-04-10 21:20:02 +0530

    String compression

---
+++ Strings/src/stringsAndArraylist/StringCompression.java
@@ -0,0 +1,50 @@
+package stringsAndArraylist;
+
+import java.util.Scanner;
+
+public class StringCompression {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		scn.close();
+		System.out.println(compression1(str));
+		System.out.println(compression2(str));
+	}
+
+	public static String compression1(String str) {
+		String s = str.substring(0, 1);
+		for (int i = 1; i < str.length(); i++) {
+			char curr = str.charAt(i);
+			char prev = str.charAt(i - 1);
+			if (curr != prev) {
+				s += curr;
+			}
+		}
+		return s;
+	}
+
+	public static String compression2(String str) {
+		String s = str.substring(0, 1);
+		int count = 1;
+		for (int i = 1; i < str.length(); i++) {
+			char curr = str.charAt(i);
+			char prev = str.charAt(i - 1);
+
+			if (curr == prev) {
+				count++;
+			} else {
+				if (count > 1) {
+					s += count;
+					count = 1;
+				}
+				s += curr;
+			}
+		}
+		if (count > 1) {
+			s += count;
+			count = 1;
+		}
+		return s;
+	}
+}
