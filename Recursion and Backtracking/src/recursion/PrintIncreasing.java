d89e6fc81baa477116ddcaba80c5c5c20e48a3af
Siddharth gulati <sid441.sg@gmail.com>
2021-04-14 22:14:21 +0530

    Print increasing

--- Recursion and Backtracking/src/recursion/PrintIncreasing.java
+++ Recursion and Backtracking/src/recursion/PrintIncreasing.java
@@ -7,15 +7,15 @@ public class PrintIncreasing {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
-		printDecreasing(n);
+		printIncreasing(n);
 		scn.close();
 	}
 
-	public static void printDecreasing(int n) {
+	public static void printIncreasing(int n) {
 		if (n == 0) {
 			return;
 		}
-		printDecreasing(n - 1);
+		printIncreasing(n - 1);
 		System.out.println(n);
 	}
 }
