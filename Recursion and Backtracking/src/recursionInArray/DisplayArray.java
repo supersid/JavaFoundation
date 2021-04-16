282de2d17a43ee671554c7c68ea63319405ce55e
Siddharth gulati <sid441.sg@gmail.com>
2021-04-16 22:19:18 +0530

    Display elements of array using recursion

---
+++ Recursion and Backtracking/src/recursionInArray/DisplayArray.java
@@ -0,0 +1,26 @@
+package recursionInArray;
+
+import java.util.Scanner;
+
+public class DisplayArray {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		scn.close();
+		displayArr(arr, 0);
+	}
+
+	public static void displayArr(int[] arr, int idx) {
+		if(idx == arr.length) {
+			return;
+		}
+		System.out.println(arr[idx]);
+		displayArr(arr, idx + 1);
+
+	}
+}
