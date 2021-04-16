8945bb24287e8848f94dd67c75387e73a16b4dcf
Siddharth gulati <sid441.sg@gmail.com>
2021-04-17 00:14:50 +0530

    Find last index of an using recursion

---
+++ Recursion and Backtracking/src/recursionInArray/LastIndex.java
@@ -0,0 +1,30 @@
+package recursionInArray;
+
+import java.util.Scanner;
+
+public class LastIndex {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		int x = scn.nextInt();
+		scn.close();
+		System.out.println(lastIndex(arr, n - 1, x));
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x){
+    	if(idx < 0) {
+    		return -1;
+    	}
+    	if(arr[idx] == x) {
+    		return idx;
+    	} else {
+    		int num = lastIndex(arr, idx - 1, x);
+    		return num;
+    	}
+    }
+}
