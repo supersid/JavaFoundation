db89480999677803afa2071b051ef8afd289b848
Siddharth gulati <sid441.sg@gmail.com>
2021-04-17 00:11:07 +0530

    Find first index of an using recursion

---
+++ Recursion and Backtracking/src/recursionInArray/FirstIndex.java
@@ -0,0 +1,36 @@
+package recursionInArray;
+
+import java.util.Scanner;
+
+public class FirstIndex {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		int x = scn.nextInt();
+		scn.close();
+		System.out.println(firstIndex(arr, 0, x));
+	}
+
+	/*
+	 * Method done by me public static int firstIndex(int[] arr, int idx, int x) {
+	 * if (idx == arr.length) { return -1; } int num = firstIndex(arr, idx + 1, x);
+	 * if(arr[idx] == x) { return idx; } else { return num; } }
+	 */
+
+	public static int firstIndex(int[] arr, int idx, int x) {
+		if (idx == arr.length) {
+			return -1;
+		}
+		if (arr[idx] == x) {
+			return idx;
+		} else {
+			int num = firstIndex(arr, idx + 1, x);
+			return num;
+		}
+	}
+}
