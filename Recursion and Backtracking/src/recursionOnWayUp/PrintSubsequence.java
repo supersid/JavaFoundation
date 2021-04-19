df2d752c9ef5219faade29d5967c22f9d7989d83
Siddharth gulati <sid441.sg@gmail.com>
2021-04-19 23:51:48 +0530

    Print subsequence using recursion on way up

---
+++ Recursion and Backtracking/src/recursionOnWayUp/PrintSubsequence.java
@@ -0,0 +1,24 @@
+package recursionOnWayUp;
+
+import java.util.Scanner;
+
+public class PrintSubsequence {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		scn.close();
+		printSS(str, "");
+	}
+
+	public static void printSS(String str, String ans) {
+		if(str.length() == 0) {
+			System.out.println(ans);
+			return;
+		}
+		char ch = str.charAt(0);
+		String remString = str.substring(1);
+		printSS(remString, ans + ch);
+		printSS(remString, ans + "");
+	}
+}
