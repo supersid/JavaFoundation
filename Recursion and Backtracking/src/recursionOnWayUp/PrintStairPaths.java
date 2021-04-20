bdd20c5d2021e858125962e38214e372c7a73eee
Siddharth gulati <sid441.sg@gmail.com>
2021-04-20 23:49:26 +0530

    Print stair paths

---
+++ Recursion and Backtracking/src/recursionOnWayUp/PrintStairPaths.java
@@ -0,0 +1,25 @@
+package recursionOnWayUp;
+
+import java.util.Scanner;
+
+public class PrintStairPaths {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		scn.close();
+		printStairPaths(n, "");
+	}
+
+	public static void printStairPaths(int n, String path) {
+		if (n == 0) {
+			System.out.println(path);
+		}
+		if (n < 0) {
+			return;
+		}
+		printStairPaths(n - 1, path + "1");
+		printStairPaths(n - 2, path + "2");
+		printStairPaths(n - 3, path + "3");
+	}
+}
