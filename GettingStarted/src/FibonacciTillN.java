52364fafb49dde996c03ecee876c7524a2493f79
Siddharth gulati <sid441.sg@gmail.com>
2021-04-25 22:14:09 +0530

    Print first n fibonacci

--- GettingStarted/src/FibonacciTillN.java
+++ GettingStarted/src/FibonacciTillN.java
@@ -1,3 +1,4 @@
+// To print fibonacci number till N
 import java.util.Scanner;
 
 public class FibonacciTillN {
@@ -5,16 +6,15 @@ public class FibonacciTillN {
 	public static void main(String[] args) {
 		try (Scanner scn = new Scanner(System.in)) {
 			int n = scn.nextInt();
-			int i = 0;
-			int j = 1;
-
+			int i = 0; // First fibonacci number
+			int j = 1; // Second fibonacci number
 			System.out.println(i);
 			System.out.println(j);
 			for (int k = 3; k <= n; k++) {
-				int c = i + j;
+				int c = i + j; // Sum of previous 2 numbers
 				System.out.println(c);
-				i = j;
-				j = c;
+				i = j; // Shift reference from second to first
+				j = c; // Shift reference from sum to second
 			}
 		}
 	}
