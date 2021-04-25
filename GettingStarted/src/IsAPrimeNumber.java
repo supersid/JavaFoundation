8ed93f9d122251051eab5346e298285a9864a4f2
Siddharth gulati <sid441.sg@gmail.com>
2021-04-25 21:33:22 +0530

    To check if entered number is prime or not prime

--- GettingStarted/src/IsAPrimeNumber.java
+++ GettingStarted/src/IsAPrimeNumber.java
@@ -4,17 +4,20 @@ public class IsAPrimeNumber {
 
 	public static void main(String[] args) {
 		try (Scanner scn = new Scanner(System.in)) {
-			int t = scn.nextInt();
+			int t = scn.nextInt(); // input for total number of values to be checked
 			for (int i = 1; i <= t; i++) {
 				int count = 0;
-				int n = scn.nextInt();
+				int n = scn.nextInt(); // take input to check for prime or not prime
+				// The loops starts from 2 as 1 divides all the natural numbers.
+				// The loop will only run till values for sqrt(n) as the factor value will not
+				// be greater than the square root value
 				for (int j = 2; j * j <= n; j++) {
 					if (n % j == 0) {
 						count++;
-						break;
+						break; // The loop will break if the entered value gets divided.
 					}
 				}
-				if (count == 0) {
+				if (count == 0) { // If the number is not divided then it is a prime number
 					System.out.println("prime");
 				} else {
 					System.out.println("not prime");
