cd8eb76f306b040bf33ca939686e621a77b025b6
Siddharth gulati <sid441.sg@gmail.com>
2021-04-26 23:44:47 +0530

    Prime factorization of a number

--- GettingStarted/src/PrimeFactorization.java
+++ GettingStarted/src/PrimeFactorization.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class PrimeFactorization {
 
 	public static void main(String[] args) {
+		// Prime factors for 56 is 2, 2, 2, 7
 		Scanner scn = new Scanner(System.in);
 		int num = scn.nextInt();
 		scn.close();
 		for(int i = 2; i * i < num; i++) {
 			while(num % i == 0) {
-				num = num / i;
+				num = num / i; // num will be 28 when (i = 2 and num = 56) initially
 				System.out.print(i + " ");
 			}
 		}
-		if(num != 1) {
+		if(num != 1) { // This is for that case when one of the factor is number itself
 			System.out.println(num);
 		}
 	}
