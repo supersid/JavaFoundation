eaea7fe3af559f1614c3a49ded0beb99a5798ea8
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 00:16:05 +0530

    Print inverted mirror triangle

--- patterns/patterns/InvertedMirrorTriangle.java
+++ patterns/patterns/InvertedMirrorTriangle.java
@@ -1,16 +1,20 @@
+//  Print the pattern shown below
+//	*	*	*	*	*	
+//		*	*	*	*	
+//			*	*	*	
+//				*	*	
+//					*
 package patterns;
 
 import java.util.Scanner;
 
-public class pattern4 {
+public class InvertedMirrorTriangle {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		scn.close();
-		
 		int space = 0;
 		int star = n;
-		
 		for(int i = 1; i <= n; i++) {
 			for(int j = 1; j <= space; j++) {
 				System.out.print("\t");
