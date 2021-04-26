8329fefe7b78afdb6f9a2909636c6778d8e60f7e
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 00:40:52 +0530

    Print fibonacci triangle

--- patterns/patterns/FibonacciTriangle.java
+++ patterns/patterns/FibonacciTriangle.java
@@ -1,17 +1,21 @@
+//  Print the pattern shown below
+//	0	
+//	1		1	
+//	2		3		5	
+//	8		13		21		34	
+//	55		89		144		233		377
 package patterns;
 
 import java.util.Scanner;
 
-public class pattern12 {
+public class FibonacciTriangle {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-
 		int n = scn.nextInt();
-
+		scn.close();
 		int n1 = 0;
 		int n2 = 1;
-
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(n1 + "	");
@@ -22,5 +26,4 @@ public class pattern12 {
 			System.out.println();
 		}
 	}
-
 }
