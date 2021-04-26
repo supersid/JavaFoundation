bcc0d63556ff3168a272521e39a4a89a850d845d
Siddharth gulati <sid441.sg@gmail.com>
2021-04-26 22:48:30 +0530

    Print digits of a number

--- GettingStarted/src/SplitDigits.java
+++ GettingStarted/src/SplitDigits.java
@@ -1,25 +1,27 @@
+/*
+ * The example here is for single iteration
+ */
 import java.util.Scanner;
 
 public class SplitDigits {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-
-		int n = scn.nextInt();
-		int temp = n;
+		int n = scn.nextInt(); // (example - 123456)
+		scn.close();
+		// Count number of digits
+		int temp = n; // temp variable to count number of digits
 		int count = 0;
 		while (temp != 0) {
 			temp = temp / 10;
-			count++;
+			count++; // 6 for (123456)
 		}
-		int div = (int) Math.pow(10, count - 1);
+		int div = (int) Math.pow(10, count - 1); // new number of same power value (100000 for 123456)
 		while (div != 0) {
-			int digit = n / div;
-			System.out.println(digit);
-
-			n = n % div;
-			div = div / 10;
+			int digit = n / div; // (1 for 123456)
+			System.out.println(digit); // (1 will be printed)
+			n = n % div; // (23456)
+			div = div / 10; // (10000)
 		}
 	}
-
 }
