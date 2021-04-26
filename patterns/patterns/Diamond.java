e2e558e3cd100a8b56d56aaa0482ef7a590b2a32
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 00:18:44 +0530

    Print diamond using stars

--- patterns/patterns/Diamond.java
+++ patterns/patterns/Diamond.java
@@ -1,33 +1,37 @@
+//  Print the pattern shown below
+//			*			
+//		*	*	*		
+//	*	*	*	*	*	
+//		*	*	*		
+//			*			
 package patterns;
 
 import java.util.Scanner;
 
-public class pattern5 {
+public class Diamond {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		scn.close();
-		
-		int space = n/2;
+		int space = n / 2;
 		int star = 1;
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= space; j++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= space; j++) {
 				System.out.print("\t");
 			}
-			for(int j = 1; j <= star; j++) {
+			for (int j = 1; j <= star; j++) {
 				System.out.print("*\t");
 			}
-			for(int j = 1; j <= space; j++) {
+			for (int j = 1; j <= space; j++) {
 				System.out.print("\t");
 			}
-			if(i <= n/2) {
+			if (i <= n / 2) {
 				star += 2;
 				space--;
 			} else {
 				star -= 2;
 				space++;
-			}			
+			}
 			System.out.println();
 		}
 	}
