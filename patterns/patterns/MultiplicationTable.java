a6354f3969f7ece1b0301aa8bd9cfb0693b52b1a
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 00:47:48 +0530

    Print multiplication table for number n

--- patterns/patterns/MultiplicationTable.java
+++ patterns/patterns/MultiplicationTable.java
@@ -1,16 +1,17 @@
+// Print multiplication table till 10 for number n
 package patterns;
 
 import java.util.Scanner;
 
-public class pattern14 {
+public class MultiplicationTable {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int n  = scn.nextInt();
+		int n = scn.nextInt();
 		scn.close();
-		
-		for(int i = 1; i <= 10; i++) {
-			System.out.println(n + " * " + i + " = " + n*i);
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " * " + i + " = " + n * i);
 		}
 	}
 }
