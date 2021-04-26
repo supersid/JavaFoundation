5f7786f3ef52a5efbed275cbf2cc8e2ced683a59
Siddharth gulati <sid441.sg@gmail.com>
2021-04-26 22:31:08 +0530

    Count digits in a number

--- GettingStarted/src/CountDigitsInANumber.java
+++ GettingStarted/src/CountDigitsInANumber.java
@@ -4,14 +4,13 @@ public class CountDigitsInANumber {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-
 		long n = scn.nextLong();
+		scn.close();
 		int count = 0;
-		while (n != 0) {
+		while (n != 0) { // Divide number by 10 to take number digit by digit (example - 50/10 = 5)
 			n = n / 10;
-			count++;
+			count++; // number of digits is referenced in this variable
 		}
 		System.out.println(count);
 	}
-
 }
