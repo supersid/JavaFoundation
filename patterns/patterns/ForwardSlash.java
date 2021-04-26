012a306682d2981c8b8471372f13ddeb9d1196db
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 00:25:41 +0530

    Print forward slash(/) using stars

--- patterns/patterns/ForwardSlash.java
+++ patterns/patterns/ForwardSlash.java
@@ -1,17 +1,26 @@
+//  Print the pattern shown below
+//					*	
+//	
+//				*	
+//		
+//			*	
+//			
+//		*	
+//				
+//	*
 package patterns;
 
 import java.util.Scanner;
 
-public class pattern8 {
+public class ForwardSlash {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		scn.close();
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				if(i+j == n+1) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i + j == n + 1) {
 					System.out.println("*\t");
 				} else {
 					System.out.print("\t");
