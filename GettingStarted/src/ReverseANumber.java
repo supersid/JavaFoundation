9c7e06495b5162c61afce177c6a26348851638c7
Siddharth gulati <sid441.sg@gmail.com>
2021-04-26 22:57:39 +0530

    Reverse a number

--- GettingStarted/src/ReverseANumber.java
+++ GettingStarted/src/ReverseANumber.java
@@ -1,17 +1,19 @@
+/*
+ * Example for this code is for single iteration
+ */
 import java.util.Scanner;
 
 public class ReverseANumber {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-
-		int n = scn.nextInt();
+		int n = scn.nextInt(); // (example - 123456)
+		scn.close();
 		while (n != 0) {
 			int div = 10;
-			int revDigit = n % div;
-			System.out.print(revDigit);
-			n = n / 10;
+			int revDigit = n % div; // (6 for 123456)
+			System.out.print(revDigit); // (Print 6)
+			n = n / 10; // (123455 for 123456)
 		}
 	}
-
 }
