b4dd44ea2827bc151fef4e0371008c42b2f824f1
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 00:09:09 +0530

    Print right triangle using stars

--- patterns/patterns/Triangle.java
+++ patterns/patterns/Triangle.java
@@ -1,20 +1,24 @@
+//  Print the pattern shown below
+//	*
+//	*	*
+//	*	*	*
+//	*	*	*	*
+//	*	*	*	*	*
 package patterns;
 
 import java.util.Scanner;
 
-public class pattern1 {
+public class Triangle {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		scn.close();
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= i; j++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
 	}
-
 }
