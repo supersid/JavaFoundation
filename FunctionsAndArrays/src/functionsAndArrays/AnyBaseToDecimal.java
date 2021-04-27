d43ab6ae3dba02419858b8a868031db203214b18
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 23:31:13 +0530

    Convert any base to decimal

--- FunctionsAndArrays/src/functionsAndArrays/AnyBaseToDecimal.java
+++ FunctionsAndArrays/src/functionsAndArrays/AnyBaseToDecimal.java
@@ -6,8 +6,8 @@ public class AnyBaseToDecimal {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
-		int b = scn.nextInt();
+		int n = scn.nextInt(); // Entered number (Example - 111001)
+		int b = scn.nextInt(); // source base (Example - 2)
 		scn.close();
 		int d = getValueIndecimal(n, b);
 		System.out.println(d);
@@ -17,12 +17,11 @@ public class AnyBaseToDecimal {
 		int rv = 0;
 		int place = 0;
 		while (n != 0) {
-			int rem = n % 10;
-			rv = rv + rem * (int) (Math.pow(b, place));
+			int rem = n % 10; // (1, 0, 0, 1, 1, 1)
+			rv = rv + rem * (int) (Math.pow(b, place));// (1*2^0 + 0*2^1 + 0*2^2 + 1*2^3 + 1*2^4 + 1*2^5)
 			place++;
-			n = n / 10;
+			n = n / 10; // (11100, 1110, 111, 11, 1)
 		}
-		return rv;
+		return rv; // (57)
 	}
-
 }
