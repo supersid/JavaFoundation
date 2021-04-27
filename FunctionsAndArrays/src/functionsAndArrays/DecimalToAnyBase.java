e8e5570c4afc91b6f241f3013823c8956099367d
Siddharth gulati <sid441.sg@gmail.com>
2021-04-27 23:28:18 +0530

    Convert decimal to any base

--- FunctionsAndArrays/src/functionsAndArrays/DecimalToAnyBase.java
+++ FunctionsAndArrays/src/functionsAndArrays/DecimalToAnyBase.java
@@ -6,22 +6,22 @@ public class DecimalToAnyBase {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
-		int b = scn.nextInt();
+		int n = scn.nextInt(); // Enter decimal number (example - 57)
+		int b = scn.nextInt(); // Enter destination base (example - 2)
 		scn.close();
 		int dn = getValueInBase(n, b);
 		System.out.println(dn);
 	}
 
 	public static int getValueInBase(int n, int b) {
-		int rv = 0;
-		int place = 0;
+		int rv = 0; // result value
+		int place = 0; // place value
 		while (n != 0) {
-			int rem = n % b;
-			rv = rv + rem * (int)(Math.pow(10, place));
-			place++;
-			n = n / b;
+			int rem = n % b; // remainder after dividing decimal number with destination base (1, 0, 0, 1, 1, 1)
+			rv = rv + rem * (int)(Math.pow(10, place)); // (1*10^0 + 0*10^1 + 0*10^2 + 1*10^3 + 1*10^4 + 1*10^5)
+			place++; // (1, 2, 3, 4, 5, 6)
+			n = n / b; // (28, 14, 7, 3, 1)
 		}
-		return rv;
+		return rv; // (111001)
 	}
 }
