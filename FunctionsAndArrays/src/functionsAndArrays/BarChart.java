bc2605c9e4ba75bb453e501f726266e21672940d
Siddharth gulati <sid441.sg@gmail.com>
2021-04-29 00:04:31 +0530

    Bar chart of stars

--- FunctionsAndArrays/src/functionsAndArrays/BarChart.java
+++ FunctionsAndArrays/src/functionsAndArrays/BarChart.java
@@ -1,9 +1,25 @@
+/*
+ * You are given a number n, representing the size of array arr.
+ * You are given n numbers, representing elements of array arr.
+ * You are required to print a bar chart representing value of array.
+ */
+// Example n = 5, element = 3, 1, 0, 7, 5
+// Example output
+//				*		
+//				*		
+//				*	*	
+//				*	*	
+//	*			*	*	
+//	*			*	*	
+//	*	*		*	*
+
 package functionsAndArrays;
 
 import java.util.Scanner;
 
 public class BarChart {
 
+	// To create a bar chart you have to find max in an array
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
@@ -18,6 +34,11 @@ public class BarChart {
 				max = arr[i];
 			}
 		}
+		/*
+		 * Set max value as the top row and go down.
+		 * Iterate over the array if you find value equal to the row then print it.
+		 * else print empty space.
+		 */ 
 		int star = max;
 		for (int i = 0; i < max; i++) {
 			for(int j = 0; j < arr.length; j++) {
