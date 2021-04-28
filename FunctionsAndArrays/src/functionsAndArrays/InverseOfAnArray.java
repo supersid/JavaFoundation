0c9a589f279cf5b38f01d1abd8261dd0d2a5166f
Siddharth gulati <sid441.sg@gmail.com>
2021-04-29 00:52:33 +0530

    Inverse of an array

--- FunctionsAndArrays/src/functionsAndArrays/InverseOfAnArray.java
+++ FunctionsAndArrays/src/functionsAndArrays/InverseOfAnArray.java
@@ -1,3 +1,11 @@
+/*
+ * You are given a number n, representing the size of array a.
+ * You are given n numbers, representing elements of array a.
+ * You are required to calculate the inverse of array a.
+ * This is similar to question reverse a number in getting started (reference - https://github.com/supersid/JavaFoundation/blob/master/GettingStarted/src/InverseOfANumber.java)
+ * The range of values is from 0 to n - 1
+ * Replace index with values
+ */
 package functionsAndArrays;
 
 import java.io.BufferedReader;
@@ -15,7 +23,7 @@ public class InverseOfAnArray {
 
 	public static int[] inverse(int[] a) {
 		int[] arrayInverse = new int[a.length];
-		for(int i = 0; i < a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			arrayInverse[a[i]] = i;
 		}
 		return arrayInverse;
