451f5beff156f3f53a9eb399f1ed06f1fe60f515
Siddharth gulati <sid441.sg@gmail.com>
2021-05-02 00:14:12 +0530

    String compression part 1 & 2

--- Strings/src/stringsAndArraylist/StringCompression.java
+++ Strings/src/stringsAndArraylist/StringCompression.java
@@ -1,3 +1,11 @@
+/*
+ * You are given a string. 
+ * You have to compress the given string in the following two ways - 
+ * First compression -> The string should be compressed such that consecutive duplicates of characters are replaced with a single character.
+   For "aaabbccdee", the compressed string will be "abcde".
+ * Second compression -> The string should be compressed such that consecutive duplicates of characters are replaced with the character and followed by the number of consecutive duplicates.
+   For "aaabbccdee", the compressed string will be "a3b2c2de2".
+ */
 package stringsAndArraylist;
 
 import java.util.Scanner;
