ee6e274953c92a61721b618462f31ee23cf547c1
Siddharth gulati <sid441.sg@gmail.com>
2021-05-02 00:20:59 +0530

    ASCII difference

--- Strings/src/stringsAndArraylist/ASCIIDifference.java
+++ Strings/src/stringsAndArraylist/ASCIIDifference.java
@@ -1,3 +1,12 @@
+/*
+ * You are given a string that contains only lowercase and uppercase alphabets. 
+ * You have to form a string that contains the difference of ASCII values of every two consecutive characters between those characters.
+ * For "abecd", the answer should be "a1b3e-2c1d", as 
+   'b'-'a' = 1
+   'e'-'b' = 3
+   'c'-'e' = -2
+   'd'-'c' = 1
+ */
 package stringsAndArraylist;
 
 import java.util.Scanner;
