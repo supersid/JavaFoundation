fed78c793754bfb0a3f659cba5abafbe5e6a153f
Siddharth gulati <sid441.sg@gmail.com>
2021-05-01 18:48:55 +0530

    Exit point in a matrix

--- 2DArrays/src/multiDimensionArrays/ExitPointMatrix.java
+++ 2DArrays/src/multiDimensionArrays/ExitPointMatrix.java
@@ -1,3 +1,11 @@
+/*
+ * You are given a number n, representing the number of rows.
+ * You are given a number m, representing the number of columns.
+ * You are given n*m numbers (1's and 0's), representing elements of 2d array a.
+ * Consider this array a maze and a player enters from top-left corner in east direction.
+ * The player moves in the same direction as long as he meets '0'. On seeing a 1, he takes a 90 deg right turn.
+ * You are required to print the indices in (row, col) format of the point from where you exit the matrix.
+ */
 package multiDimensionArrays;
 
 import java.util.Scanner;
@@ -24,7 +32,7 @@ public class ExitPointMatrix {
 		int direction = 0;
 
 		while (true) {
-			direction = (direction + arr[i][j]) % 4; // whenever we get value as '1' we have to change the direction
+			direction = (direction + arr[i][j]) % 4; // whenever we get arr[i][j] as '1' we have to change the direction
 			if (direction == 0) { // east direction
 				j++;
 			} else if (direction == 1) { // south direction
@@ -34,6 +42,7 @@ public class ExitPointMatrix {
 			} else if (direction == 3) { // north direction
 				i--;
 			}
+			// We have to move until we reach an exit.
 			if (i == arr.length) {
 				i--;
 				break;
