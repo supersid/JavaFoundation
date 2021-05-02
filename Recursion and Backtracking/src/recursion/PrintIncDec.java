82199bfe662be3470eb3adb8b0681446a9f6b90e
Siddharth gulati <sid441.sg@gmail.com>
2021-05-02 15:13:23 +0530

    Print increasing-decreasing

--- Recursion and Backtracking/src/recursion/PrintIncDec.java
+++ Recursion and Backtracking/src/recursion/PrintIncDec.java
@@ -1,3 +1,15 @@
+// Input -> 5
+// Output
+// 5
+// 4
+// 3
+// 2
+// 1
+// 1
+// 2
+// 3
+// 4
+// 5
 package recursion;
 
 import java.util.Scanner;
@@ -6,16 +18,16 @@ public class PrintIncDec {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
-		printDecreasing(n);
+		printIncreasingDecreasing(n);
 		scn.close();
 	}
 
-	public static void printDecreasing(int n) {
+	public static void printIncreasingDecreasing(int n) {
 		if (n == 0) {
 			return;
 		}
-		System.out.println(n);
-		printDecreasing(n - 1);
-		System.out.println(n);
+		System.out.println(n); // going up
+		printIncreasingDecreasing(n - 1);
+		System.out.println(n); // coming down
 	}
 }
