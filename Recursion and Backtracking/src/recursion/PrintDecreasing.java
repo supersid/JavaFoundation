66b14af7221ff8157b4eeae4d7b8b828d69f2c71
Siddharth gulati <sid441.sg@gmail.com>
2021-05-02 14:51:05 +0530

    Print decreasing

--- Recursion and Backtracking/src/recursion/PrintDecreasing.java
+++ Recursion and Backtracking/src/recursion/PrintDecreasing.java
@@ -1,3 +1,15 @@
+/*
+ * Faith -> n(5) = 5,4,3,2,1
+ * Expectation -> 5 + n(4)
+ */
+// Example
+// Input - 5
+// Output
+// 5
+// 4
+// 3
+// 2
+// 1
 package recursion;
 
 import java.util.Scanner;
