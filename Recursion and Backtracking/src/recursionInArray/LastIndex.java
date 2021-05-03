4fe7de686ee63aae945bd3c7366e417edc748959
Siddharth gulati <sid441.sg@gmail.com>
2021-05-03 23:46:08 +0530

    Print last index using recursion

--- Recursion and Backtracking/src/recursionInArray/LastIndex.java
+++ Recursion and Backtracking/src/recursionInArray/LastIndex.java
@@ -18,10 +18,10 @@ public class LastIndex {
 
     public static int lastIndex(int[] arr, int idx, int x){
     	if(idx < 0) {
-    		return -1;
+    		return -1; // if number not found and there are no elements left return -1
     	}
     	if(arr[idx] == x) {
-    		return idx;
+    		return idx; // if number if found return index
     	} else {
     		int num = lastIndex(arr, idx - 1, x);
     		return num;
