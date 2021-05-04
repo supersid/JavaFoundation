1b527ea506117bf00fb11947b63b7bbc8943ac32
Siddharth gulati <sid441.sg@gmail.com>
2021-05-04 23:26:33 +0530

    Get subsequence using recursion in arraylist

--- Recursion and Backtracking/src/recursionInArrayList/GetSubsequence.java
+++ Recursion and Backtracking/src/recursionInArrayList/GetSubsequence.java
@@ -1,3 +1,6 @@
+// Example
+// Input str = abc
+// Output -> [, c, b, bc, a, ac, ab, abc]
 package recursionInArrayList;
 
 import java.util.ArrayList;
@@ -8,26 +11,27 @@ public class GetSubsequence {
 	public static void main(String[] args) throws Exception {
 		Scanner scn = new Scanner(System.in);
 		String str = scn.next();
+		scn.close();
 		ArrayList<String> res = gss(str);
 		System.out.println(res);
 	}
 
 	public static ArrayList<String> gss(String str) {
-		if(str.length() == 0) {
+		if (str.length() == 0) {
 			ArrayList<String> baseCase = new ArrayList<String>();
-			baseCase.add("");
+			baseCase.add(""); // when there is no value just add blank
 			return baseCase;
 		}
-		char ch = str.charAt(0);
-		String remString = str.substring(1);
+		char ch = str.charAt(0); // a
+		String remString = str.substring(1); // bc
 		ArrayList<String> rss = gss(remString);
 		ArrayList<String> myss = new ArrayList<String>();
-		
+
 		for (String string : rss) {
-			myss.add("" + string);
+			myss.add("" + string); // [---, --c, -b-, -bc]
 		}
 		for (String string : rss) {
-			myss.add(ch + string);
+			myss.add(ch + string); // [---, --c, b--, -bc, a--, a-c, ab-, abc]
 		}
 		return myss;
 	}
