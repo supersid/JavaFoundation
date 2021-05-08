53027185b1ef7fe7c1f7f7a9797a5ef2565ce622
Siddharth gulati <sid441.sg@gmail.com>
2021-05-08 21:44:32 +0530

    Print maze paths with random jumps

--- Recursion and Backtracking/src/recursionOnWayUp/PrintMazePathWithJumps.java
+++ Recursion and Backtracking/src/recursionOnWayUp/PrintMazePathWithJumps.java
@@ -1,3 +1,28 @@
+//	Example
+//	Input -> n = 3, m = 3
+//	Output
+//	h1h1v1v1
+//	h1h1v2
+//	h1v1h1v1
+//	h1v1v1h1
+//	h1v1d1
+//	h1v2h1
+//	h1d1v1
+//	h2v1v1
+//	h2v2
+//	v1h1h1v1
+//	v1h1v1h1
+//	v1h1d1
+//	v1h2v1
+//	v1v1h1h1
+//	v1v1h2
+//	v1d1h1
+//	v2h1h1
+//	v2h2
+//	d1h1v1
+//	d1v1h1
+//	d1d1
+//	d2
 package recursionOnWayUp;
 
 import java.util.Scanner;
@@ -25,6 +50,7 @@ public class PrintMazePathWithJumps {
 		if (sr > dr || sc > dc) {
 			return;
 		}
+		// variable ms is used because the count of steps is not fixed
 		for (int ms = 1; ms <= dc - sc; ms++) {
 			printMazePaths(sr, sc + ms, dr, dc, psf + "h" + ms);
 		}
